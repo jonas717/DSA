5abac99255bf73c34d19a5d6f2584244922762bd
Jonas Werner <wernerjonas77@gmail.com>
2021-07-07 15:14:05 +0200

    corrections of error

--- DSA.py
+++ DSA.py
@@ -1,17 +1,17 @@
 import random as r
 
+
 # implementation of DSA
 
-def num_is_prime(num): 
+def num_is_prime(num):
     if (num <= 3) or (num % 2 == 0):
         return num == 2 or num == 3
 
     divisor = 3
-    while (divisor <= num**0.5) and (num % divisor != 0):
+    while (divisor <= num ** 0.5) and (num % divisor != 0):
         divisor += 2
 
-    return num % divisor != 0 
-   
+    return num % divisor != 0
 
 
 def find_prime_pair(min, max):
@@ -24,7 +24,7 @@ def find_prime_pair(min, max):
 
     q = 1
     while not (p_decremented % q == 0 and num_is_prime(q)):
-        q = r.randint(min/1000, p_decremented)
+        q = r.randint(min / 100, p_decremented)
 
     return p, q
 
@@ -36,6 +36,7 @@ def mod_inv(a, mod):
             break
         i += 1
     return i
+
 
 # mod exponentiation with right to left binary method
 def mod_pow(a, ex, mod):
@@ -49,7 +50,7 @@ def mod_pow(a, ex, mod):
     while ex > 0:
         if (ex % 2 == 1):
             res = ((res % mod) * (a % mod)) % mod
-        
+
         ex = ex >> 1
         a = (a * a) % mod
 
@@ -58,40 +59,45 @@ def mod_pow(a, ex, mod):
 
 # find element g
 def find_element_g(p, q):
-    g = 0
     for h in range(2, p):
-        ex = int((p-q)/q)
-        g = mod_pow(h, ex, p)
+        g = mod_pow(h, int((p - 1) / q), p)
         if g > 1:
             return g
 
 
 # generate key pair
 def generate_key_pair(p, q, g):
-    sk = r.randint(1, q-1)
-    
+    sk = r.randint(1, q - 1)
+
     pk = mod_pow(g, sk, p)
     return sk, pk
 
+
 def pick_k(q):
-    return r.randint(1, q-1)
+    return r.randint(1, q - 1)
+
 
 def calculate_r(g, k, p, q):
     return mod_pow(g, k, p) % q
 
+
 def calculate_s(k, r, sk, hash, q):
     return (hash + r * sk) * mod_inv(k, q) % q
+
 
 # verify
 
 def calculate_w(s, q):
     return mod_inv(s, q)
 
+
 def calculate_u(w, hash, q):
     return (w * hash) % q
 
+
 def calculate_v(w, r, q):
     return (w * r) % q
+
 
 def calculate_z(g, u, pk, v, p, q):
     return ((mod_pow(g, u, p) * mod_pow(pk, v, p)) % p) % q
--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import DSA
 import hashlib
 
-str = "Hello my name is Jonas."
+message = "Hello my name is Jonas."
 
-hash = int(hashlib.sha256(str.encode()).hexdigest(), 16)
-
+message_hash = int(hashlib.sha256(message.encode()).hexdigest(), 16)
+print(message_hash)
 # find prime pair in given range
-p,q = DSA.find_prime_pair(10000, 100000)
+p, q = DSA.find_prime_pair(1000, 90000)
 
 print(f"Prime p is {p} and prime q is {q}.")
 
@@ -20,8 +20,6 @@ print(f"Element g is {g}")
 sk, pk = DSA.generate_key_pair(p, q, g)
 
 print(f"Secret key is {sk} and the public key is {pk}")
-
-print(hash)
 
 # signature
 k = 0
@@ -40,27 +38,25 @@ while s == 0:
         iterations += 1
 
     if iterations_outside > 20:
-            break
-    s = DSA.calculate_s(k, r, sk, hash, q)
+        break
+    s = DSA.calculate_s(k, r, sk, message_hash, q)
     iterations_outside += 1
-
 
 signature = (k, r, s)
 
-if signature[0] == 0 or signature[1] == 0:
+if signature[1] == 0 or signature[2] == 0:
     print(f"Could not be signed. Repeat the algorithm.")
 else:
     print(f"The signature is {signature}.")
-# validation
+    # validation
     w = DSA.calculate_w(s, q)
-    u = DSA.calculate_u(w, hash, q)
+    u = DSA.calculate_u(w, message_hash, q)
     v = DSA.calculate_v(w, r, q)
     z = DSA.calculate_z(g, u, pk, v, p, q)
-    
+
     print(f"z is {z}")
 
     if z == signature[1]:
         print(f"Signature is verified with r = {signature[1]} and z = {z}.")
-    else: 
+    else:
         print(f"Signature is not valid with r = {signature[1]} and z = {z}.")
-
